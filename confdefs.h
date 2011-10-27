4ea9d5fa4b455d92ae83925e72645025a14044fa
Norihiro Kamae <norihiro@nagater.net>
2011-10-27 23:20:45 +0900

    clean repository

--- confdefs.h
+++
@@ -1,8 +0,0 @@
-
-#define PACKAGE_NAME "beep"
-#define PACKAGE_TARNAME "beep"
-#define PACKAGE_VERSION "1.3.2"
-#define PACKAGE_STRING "beep 1.3.2"
-#define PACKAGE_BUGREPORT "norihiro@nagater.net"
-#define PACKAGE "beep"
-#define VERSION "1.3.2"
